0db81733c6eaba78deea80a633fa8cdf65a74f08
Jose Perez <69835113+Kami62176@users.noreply.github.com>
2023-11-14 12:56:28 -0700

    adding content

---
+++ src/main/java/test/docker/testingdockerization/HomeRestController.java
@@ -0,0 +1,7 @@
+/**
+    @author jperezbenitez
+    @createdOn 11/14/2023 at 12:51 PM
+    @projectName testingDockerization
+ @packageName test.docker.testingdockerization;*/
+package test.docker.testingdockerization;public class HomeRestController {
+}
---
+++ src/main/java/test/docker/testingdockerization/SecurityConfig.java
@@ -0,0 +1,68 @@
+/**
+ * @author jperezbenitez
+ * @createdOn 11/8/2023 at 2:01 PM
+ * @projectName securitySpring
+ * @packageName csc280.perezbenitez.jose.securityspring;
+ */
+package csc280.perezbenitez.jose.securityspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("jose")
+                .password(passwordEncoder().encode("hunter")).build());
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(a -> a
+                        .requestMatchers("/index.html").permitAll()
+                        .requestMatchers("/agent/notSecret").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.sessionManagement(sm -> sm.disable());
+        http.csrf(csrf -> csrf.disable()); //disables cross site request forgery checking
+
+        http.cors(cors-> cors.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(List.of("*"));
+            config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE"));
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowCredentials(true);
+            return config;
+        }));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
